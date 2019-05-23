34ecfa1f39c3548bf1f299a267376070581f618c
moxDroid Labs Inc <pritam.world@gmail.com>
2019-05-23 18:56:54 -0400

    [Pritesh] Update Welcome message to Java

--- src/com/lambton/CollegeSystem.java
+++ src/com/lambton/CollegeSystem.java
@@ -3,6 +3,6 @@ package com.lambton;
 public class CollegeSystem
 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, Java");
     }
 }
