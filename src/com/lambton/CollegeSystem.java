1cb29a95de0976f730ddd769d7912555a319e117
moxDroid Labs Inc <pritam.world@gmail.com>
2019-05-23 19:31:13 -0400

    [Pritesh] New sum logic added

--- src/com/lambton/CollegeSystem.java
+++ src/com/lambton/CollegeSystem.java
@@ -5,5 +5,14 @@ public class CollegeSystem
     public static void main(String[] args)
     {
         System.out.println("Hello, Java");
+
+        int sum = 0;
+
+        for(int i= 1; i <= 10; i++)
+        {
+            sum = sum + i;
+        }
+
+        System.out.println("SUM of 1 to 10 is : " + sum);
     }
 }
